b30373888dde62adb3177ceb54c49564e113029b
Kui LIU <brucekuiliu@gmail.com>
2017-03-17 15:06:14 +0100

    Add some comments, and rename some variables.

--- src/main/java/edu/lu/uni/App.java
+++ src/main/java/edu/lu/uni/App.java
@@ -26,53 +26,67 @@ public class App {
 		}
 	}
 
+	/**
+	 * Append zero to the integer vectors of which sizes are less than the max size,
+	 * to make all the lengths of vectors consistent and equal to the max size.
+	 * @throws IOException
+	 */
 	public void appendZero() throws IOException {
-		String inputFolderPath = Configuration.ENCODED_METHOD_BODY_FILE_PATH;
-		String fileExtension = Configuration.TOKEN_FILE_EXTENSION;
+		String inputFilePath = Configuration.ENCODED_METHOD_BODY_FILE_PATH;
+		String inputFileExtension = Configuration.STRING_DATA_FILE_EXTENSION;
+		List<File> integerVectorsFiles = FileHelper.getAllFiles(inputFilePath, inputFileExtension);
+
+		String outputFileExtension = Configuration.DIGITAL_DATA_FILE_EXTENSION;
 		String outputFilePath = Configuration.DATA_APPZENDED_ZERO;
-		List<File> integerVectorsFiles = FileHelper.getAllFiles(inputFolderPath, fileExtension);
-		
-		// Clear existing output data generated at last time.
+		// Clear existing output data generated at the last time.
 		FileHelper.deleteDirectory(outputFilePath);
 		
 		ZeroAppender appender = new ZeroAppender();
 		for (File file : integerVectorsFiles) {
-			appender.appendZeroForVectors(file, inputFolderPath, outputFilePath, fileExtension, Configuration.SIZE_FILE_EXTENSION);
+			appender.appendZeroForVectors(file, inputFilePath, outputFilePath, inputFileExtension, outputFileExtension);
 		}
 	}
 
+	/**
+	 * Standardize the data of integer vectors.
+	 */
 	public void standardizeData() {
-		String inputFolderPath = Configuration.ENCODED_METHOD_BODY_FILE_PATH;
-		String fileExtension = Configuration.TOKEN_FILE_EXTENSION;
+		String inputFilePath = Configuration.ENCODED_METHOD_BODY_FILE_PATH;
+		String fileExtension = Configuration.STRING_DATA_FILE_EXTENSION;
+		List<File> integerVectorsFiles = FileHelper.getAllFiles(inputFilePath, fileExtension);
+
+		String outputFileExtension = Configuration.DIGITAL_DATA_FILE_EXTENSION;
 		String outputFilePath = Configuration.DATA_STANDARDIZED;
-		List<File> integerVectorsFiles = FileHelper.getAllFiles(inputFolderPath, fileExtension);
-		
-		// Clear existing output data generated at last time.
+		// Clear existing output data generated at the last time.
 		FileHelper.deleteDirectory(outputFilePath);
 		
 		for (File file : integerVectorsFiles) {
-			Standardization s = new Standardization(file, outputFilePath, Configuration.TOKEN_FILE_EXTENSION, Configuration.SIZE_FILE_EXTENSION);
+			Standardization s = new Standardization(file, outputFilePath, fileExtension, outputFileExtension);
 			s.standardize();
 		}
 	}
 
+	/**
+	 * Extract features of method bodies by deep learning with the CNN algorithm.
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public void extractFeatureWithCNN() throws IOException, InterruptedException {
-		String fileExtension = Configuration.SIZE_FILE_EXTENSION;
-		List<File> files = FileHelper.getAllFiles(Configuration.DATA_APPZENDED_ZERO, fileExtension);
-		files.addAll(FileHelper.getAllFiles(Configuration.DATA_STANDARDIZED, fileExtension));
+		String fileExtension = Configuration.DIGITAL_DATA_FILE_EXTENSION;
+		List<File> inputFiles = FileHelper.getAllFiles(Configuration.DATA_APPZENDED_ZERO, fileExtension); // normal data.
+		inputFiles.addAll(FileHelper.getAllFiles(Configuration.DATA_STANDARDIZED, fileExtension));        // standardized data.
 		
 		String outputPath = Configuration.DATA_EXTRACTED_FEATURE;
-		
-		// Clear existing output data generated at last time.
+		// Clear existing output data generated at the last time.
 		FileHelper.deleteDirectory(outputPath);
 		
-		for (File file : files) {
-			String fileName = file.getName();
+		for (File inputFile : inputFiles) {
+			String fileName = inputFile.getName();
 			int sizeOfVector = Integer.parseInt(fileName.substring(fileName.lastIndexOf("=") + 1, fileName.lastIndexOf(fileExtension)));
 			int batchSize = 1000;
 			int sizeOfFeatureVector = 100;
 			
-			FeatureExtractor extractor = new FeatureExtractor(file, sizeOfVector, batchSize, sizeOfFeatureVector);
+			FeatureExtractor extractor = new FeatureExtractor(inputFile, sizeOfVector, batchSize, sizeOfFeatureVector);
 			extractor.setOutputPath(outputPath);
 			
 //			extractor.setNumberOfEpochs(1);
@@ -83,4 +97,5 @@ public class App {
 			extractor.extracteFeaturesWithCNN(); 
 		}
 	}
+
 }
