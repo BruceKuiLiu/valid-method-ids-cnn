1081a77f8bda8eefefbf6c8882999c0ed7167487
Kui LIU <brucekuiliu@gmail.com>
2017-04-13 11:05:01 +0200

    Modify path.

--- src/main/java/edu/lu/uni/Configuration.java
+++ src/main/java/edu/lu/uni/Configuration.java
@@ -22,7 +22,7 @@ public class Configuration {
 //		TOKEN_TYPES.add(TokenType.SEPRATED_AST_NODE_NAME_AND_RAW_TOKEN);
 ////		TOKENTYPES.add(TokenType.STATEMENT_NODE_NAME_WITH_RAW_TOKEN);
 //	}
-	public static final String TOKENIZATION_OUTPUT_PATH = "../OUTPUT/tokenization/"; // the file path of output
+	public static final String TOKENIZATION_OUTPUT_PATH = "OUTPUT/tokenization/"; // the file path of output
 	public static final String STRING_DATA_FILE_EXTENSION = ".list";  // the file extension of string data.
 	public static final String DIGITAL_DATA_FILE_EXTENSION = ".csv";  // the file extension of digital data.
 	
@@ -37,16 +37,16 @@ public class Configuration {
 	 * The methods, of which the size of token vectors is less than or equals to the max size, will be selected as latter data.
 	 */
 //	public static final MaxSizeType maxSizeType = MaxSizeType.ThirdQuarter;  
-	public static final String ENCODED_METHOD_BODY_FILE_PATH = "../OUTPUT/encoding/encoded_method_bodies/";  // output
-	public static final String SELECTED_METHOD_BODY_PATH = "../OUTPUT/encoding/selected_method_body/";       // output
-	public static final String SELECTED_SOURCE_CODE_PATH = "../OUTPUT/encoding/selected_source_code/";       // output
+	public static final String ENCODED_METHOD_BODY_FILE_PATH = "OUTPUT/encoding/encoded_method_bodies/";  // output
+	public static final String SELECTED_METHOD_BODY_PATH = "OUTPUT/encoding/selected_method_body/";       // output
+	public static final String SELECTED_SOURCE_CODE_PATH = "OUTPUT/encoding/selected_source_code/";       // output
 	
 	// token embedding with word2vec
 	public static final int SIZE_OF_EMBEDDED_VECTOR = 300;
 	public static final int MIN_WORD_FREQUENCY = 2;
-	public static final String PREPROCESSED_TOKEN_FILE_PATH = "../OUTPUT/embedding/data_preprocess/";
-	public static final String EMBEDDED_TOKEN_FILE_PATH = "../OUTPUT/embedding/token_map/";
-	public static final String EMBEDDED_DATA_FILE_PATH = "../OUTPUT/data_for_CNN/embedded_data/";
+	public static final String PREPROCESSED_TOKEN_FILE_PATH = "OUTPUT/embedding/data_preprocess/";
+	public static final String EMBEDDED_TOKEN_FILE_PATH = "OUTPUT/embedding/token_map/";
+	public static final String EMBEDDED_DATA_FILE_PATH = "OUTPUT/data_for_CNN/embedded_data/";
 		
 	
 	/**
@@ -54,8 +54,8 @@ public class Configuration {
 	 */
 	public static final int BATCH_SIZE = 200;
 	public static final int SIZE_OF_FEATURE_VECTOR = 300; // size of extracted feature vectors
-	public static final String DATA_APPZENDED_ZERO = "../OUTPUT/data_for_CNN/append_zero/";        // file path of output
-	public static final String DATA_STANDARDIZED = "../OUTPUT/data_for_CNN/standardized_data/";    // file path of output
-	public static final String DATA_EXTRACTED_FEATURE = "../OUTPUT/CNN_extracted_feature_GPU/";        // file path of output
+	public static final String DATA_APPZENDED_ZERO = "OUTPUT/data_for_CNN/append_zero/";        // file path of output
+	public static final String DATA_STANDARDIZED = "OUTPUT/data_for_CNN/standardized_data/";    // file path of output
+	public static final String DATA_EXTRACTED_FEATURE = "OUTPUT/CNN_extracted_feature_GPU/";        // file path of output
 	
 }
